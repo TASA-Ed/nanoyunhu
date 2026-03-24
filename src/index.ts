d8f3bafe12dfdd61cb7b626e133d0601fa87ad7c
ccd2s <tasaedem@outlook.com>
2026-03-24 11:23:48 +0800

    feat(Entrypoint): 添加启动花字 版本号改为从 package 导入

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import type { AppConfig } from "./types.ts";
 import { initLogger, Logger } from "./utils/logger.ts";
 import { main } from "./nanoYunHu/main.ts";
 import type { TokenTestSuccess } from "./nanoYunHu/token_test.ts";
+import AppPackage from "../package.json" with { type: 'json' };
 
-export const Version = [0, 3, 0] as const;
+export const Version = AppPackage.version.split(".");
 export const AppName = "NanoYunHu" as const;
 
 declare global {
@@ -24,19 +25,23 @@ declare global {
  * @description 注意：先运行此函数！主函数会自行运行！
  */
 export async function index(): Promise<void> {
-	console.info(styleText(["blue", "bold"], `\n| ${AppName} |\n`));
-	console.info("初始化...\n");
+	console.info(styleText(["blue", "bold"], `    _   __   ___     _   __   ____ `));
+	console.info(styleText(["blue", "bold"], `   / | / /  /   |   / | / /  / __ \\`));
+	console.info(styleText(["blue", "bold"], `  /  |/ /  / /| |  /  |/ /  / / / /`));
+	console.info(styleText(["blue", "bold"], ` / /|  /  / ___ | / /|  /  / /_/ / `));
+	console.info(styleText(["blue", "bold"], `/_/ |_/  /_/  |_|/_/ |_/   \\____/  \n`));
+	console.info(`${AppName} ${Version.join(".")}\n`);
 
 	const log = new Logger({ prefix: "Entrypoint" });
 
 	try {
+		log.info("初始化...");
 		global.appConfig = loadConfigOnStarting();
 
 		initLogger(global.appConfig.logger);
 
 		log.info("配置加载成功。");
 		log.debug("已加载配置:", global.appConfig);
-		log.info(`${AppName} 版本：${Version.join(".")}`);
 		log.info(`启动中...`);
 
 		await main();
